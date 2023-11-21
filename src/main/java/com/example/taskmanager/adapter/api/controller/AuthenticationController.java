7352f7efed5f24c099563a5bf83d0808f9a84f4d
Ricardo Manoel <rdomanoel@gmail.com>
2023-11-21 14:02:46 +0000

    dev: auth controller layer

---
+++ src/main/java/com/example/taskmanager/adapter/api/controller/AuthenticationController.java
@@ -0,0 +1,14 @@
+package com.example.taskmanager.adapter.api.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthenticationController {
+    @PostMapping("/login")
+    void login() {
+
+    }
+}
---
+++ src/main/java/com/example/taskmanager/adapter/configuration/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.example.taskmanager.adapter.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/users").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
+}
